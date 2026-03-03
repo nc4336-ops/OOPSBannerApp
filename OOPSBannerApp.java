2cde0a3083928fce70d341668603253577269d98
nc4336-ops <nc4336@srmist.edu.in>
2026-03-03 23:17:06 +0530

    Initial commit: add HelloWorld Java file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,33 @@
-/**
- * OOPSBannerApp
- *
- * A simple Java application that prints "OOPS" to the console.
- * This represents UC1 of the OOPS Banner App.
- *
- * @author YourName
- * @version 1.0
- */
-public class OOPSBannerApp {
+public class OOPSBannerUC2 {
 
-    /**
-     * Main method - Entry point of the application
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Line 1
+        System.out.println(
+                "  *****  " + "   *****  " + "  ******* " + "  ******* ");
+
+        // Line 2
+        System.out.println(
+                " *     * " + "  *     * " + "  *      *" + " *       *");
+
+        // Line 3
+        System.out.println(
+                " *     * " + "  *     * " + "  *      *" + " *        ");
+
+        // Line 4
+        System.out.println(
+                " *     * " + "  *     * " + "  ******* " + "  ******* ");
+
+        // Line 5
+        System.out.println(
+                " *     * " + "  *     * " + "  *       " + "        * ");
+
+        // Line 6
+        System.out.println(
+                " *     * " + "  *     * " + "  *       " + " *      * ");
+
+        // Line 7
+        System.out.println(
+                "  *****  " + "   *****  " + "  *       " + "  ******* ");
     }
 }
