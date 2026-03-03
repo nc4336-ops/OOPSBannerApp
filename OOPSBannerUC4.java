88c159f45b4c7815f17532e25cba1d8cf54ad984
nc4336-ops <nc4336@srmist.edu.in>
2026-03-03 23:49:01 +0530

    Added UC4 - Using String Array and Loop

---
+++ OOPSBannerUC4.java
@@ -0,0 +1,56 @@
+public class OOPSBannerUC4 {
+
+    public static void main(String[] args) {
+
+        // Step 1: Create String Array to store banner lines
+        String[] banner = new String[7];
+
+        // Step 2: Populate each line using String.join()
+        banner[0] = String.join("",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** ");
+
+        banner[1] = String.join("",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ");
+
+        banner[2] = String.join("",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       ");
+
+        banner[3] = String.join("",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  ");
+
+        banner[4] = String.join("",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * ");
+
+        banner[5] = String.join("",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * ");
+
+        banner[6] = String.join("",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  ");
+
+        // Step 3: Use Enhanced For Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
