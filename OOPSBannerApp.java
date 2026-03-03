ba50c82a7f84058d4bb91e36177720ece49f68a2
nc4336-ops <nc4336@srmist.edu.in>
2026-03-03 10:40:39 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ *
+ * A simple Java application that prints "OOPS" to the console.
+ * This represents UC1 of the OOPS Banner App.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
